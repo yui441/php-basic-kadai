f004fa700b2d26043a7539f4c5b8761a535439bc
yui441 <yui.yui.yui.n.18@gmail.com>
2024-12-20 01:08:53 +0900

    kadai_011をアップロード

---
+++ kadai_011/foreach.php
@@ -0,0 +1,9 @@
+<?php
+
+$vegetable_datas = ['名前' => '玉ねぎ','値段' => '200','産地' => '北海道'];
+
+foreach ($vegetable_datas as $vegetable_data => $value){
+    echo "{$vegetable_data} : {$value}<br>";
+
+}
+?>
